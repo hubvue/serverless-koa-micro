9cd2715c984599780725817b35f57547360ed650
kim <kimwangchong_i@didiglobal.com>
2019-10-09 20:27:46 +0800

    [chore]

--- test/api.spec.js
+++ test/api.spec.js
@@ -2,16 +2,19 @@ const supertest = require('supertest')
 const app = require('../src/apps/api')
 describe('API', () => {
   let request
+  let http
+
   beforeEach(() => {
-    request = supertest(app.listen())
-  })
-  afterEach(() => {
-    app.close()
+    http = app.listen()
+    request = supertest(http)
   })
   it('should get api', done => {
-    request.get('/api/test').expect(200, (err, res) => {
-      expect(res.body.data).toHaveLength(100)
-      done()
-    })
+    request
+      .get('/api/test')
+      .expect(200)
+      .end((err, res) => {
+        expect(res.body.data).toHaveLength(100)
+        done()
+      })
   })
 })
--- test/todo.spec.js
+++ test/todo.spec.js
@@ -3,16 +3,19 @@ const app = require('../src/apps/todo')
 
 describe('Todo', () => {
   let request
+  let http
   beforeEach(() => {
-    request = supertest(app.listen())
+    http = app.listen()
+    request = supertest(http)
   })
-  afterEach(() => {
-    app.close()
-  })
+
   it('should get todo', done => {
-    request.get('/todo/todos').expect(200, (err, res) => {
-      expect(res.body.data).toHaveLength(2)
-      done()
-    })
+    request
+      .get('/todo/todos')
+      .expect(200)
+      .end((err, res) => {
+        expect(res.body.data).toHaveLength(2)
+        done()
+      })
   })
 })
