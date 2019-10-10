c35f642a43f204d8f13290cb2041f99b50cf149d
kim <kimwangchong_i@didiglobal.com>
2019-10-10 14:20:36 +0800

    chore: add pre-applypatch workflow

--- husky.config.js
+++ husky.config.js
@@ -4,6 +4,7 @@ module.exports = {
     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
     'prepare-commit-mgs':
       'conventional-changelog -p angular -i CHANGELOG.md  -s && git add CHANGELOG.md',
-    'pre-push': 'npm run eslint'
+    'pre-push': 'npm run eslint',
+    'pre-applypatch': 'echo 123'
   }
 }
