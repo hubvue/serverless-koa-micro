ee6001b7d9474d8666b0828a8af2abf2718115f4
kim <kimwangchong_i@didiglobal.com>
2019-10-10 14:13:37 +0800

    chore(husky): move changelog to husky

--- husky.config.js
+++ husky.config.js
@@ -2,6 +2,8 @@ module.exports = {
   hooks: {
     'pre-commit': 'npm run eslint',
     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
-    'pre-push': 'npm run test'
+    'prepare-commit-mgs':
+      'conventional-changelog -p angular -i CHANGELOG.md  -s && git add CHANGELOG.md',
+    'pre-push': 'npm run eslint'
   }
 }
